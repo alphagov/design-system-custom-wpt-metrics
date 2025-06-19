514a5587b02c689f14cfaac90d5041c2d6f19759
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2025-06-19 15:15:32 +0100

    Improve logo detection

--- functions/logo.js
+++ functions/logo.js
@@ -1,21 +1,24 @@
 function getLogo() {
-   const $svg = document.querySelector('.govuk-header__logotype')
-    if (!$svg) {
-        return 'unknown'
-    }
+  const $svg = document.querySelector('.govuk-header__logotype')
 
-    /** NEEDS WORK – test against all versions of header SVG **/
-    switch (true) {
-        case $svg.querySelector('circle.govuk-logo-dot') !== null:
-            return 'middot'
-        case $svg.querySelector('path[d^="M13.4"]') !== null:
-        case $svg.querySelector('path[d^="M25"]') !== null:
-            return 'stedwards'
-        case $svg.querySelector('path[d^="M22.6"]') !== null:
-            return 'tudor'
-        default:
-            return 'unknown'
-    }
+  if (!$svg) {
+    return 'unknown'
+  }
+
+  switch (true) {
+    case $svg.querySelector('circle.govuk-logo-dot') !== null:
+      return 'middot'
+    case $svg.querySelector('path[d^="M33.1,"]') !== null: // v5.10.0+
+    case $svg.querySelector('path[d^="M22.6 10.4c-1"]') !== null: // v5.1.0+
+      return 'tudor'
+    case $svg.querySelector('path[d^="M88.6"]') !== null: // v5.10.0+
+    case $svg.querySelector('path[d^="M6.7 12.2c1 "]') !== null: // v5.1.0+
+    case $svg.querySelector('path[d^="M25 30.2c3.5"]') !== null: // v4.7.0+
+      return 'stedwards'
+
+    default:
+      return 'unknown'
+  }
 }
 
 if (typeof exports === "object") {
