efaf69fe31d8dfe1a5346b7b6e0a1955ece0999a
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2025-07-02 18:29:11 +0100

    Improve framework detection
    For GOV.UK Frontend, additionally check for the `govuk-template__body` class on the `<body>` element.
    
    For the legacy frameworks, additionally check for the header from GOV.UK Template.
    
    Allow for the possibility that both GOV.UK Frontend and the legacy frameworks are in use at the same time.
    
    Use `Array.prototype.some` to lazy evaluate the various checks.

--- metrics/framework.js
+++ metrics/framework.js
@@ -1,19 +1,24 @@
 module.exports = function () {
-  if (
+  const usesFrontend = [
     window
       .getComputedStyle(document.documentElement)
-      .getPropertyValue('--govuk-frontend-version')
-  ) {
-    return 'GOV.UK Frontend'
-  }
+      .getPropertyValue('--govuk-frontend-version'),
+    document.body.classList.contains('govuk-template__body')
+  ].some((result) => !!result)
 
-  if (document.documentElement.classList.contains('govuk-template')) {
-    return 'GOV.UK Frontend'
-  }
+  const usesLegacy = [
+    document.getElementById('skiplink-container'),
+    document.querySelector('.header-wrapper .header-global .header-logo')
+  ].some((result) => !!result)
 
-  if (document.getElementById('skiplink-container')) {
-    return 'GOV.UK Template / Frontend Toolkit / Elements'
+  switch (true) {
+    case usesFrontend && usesLegacy:
+      return 'GOV.UK Frontend + GOV.UK Template / Frontend Toolkit / Elements'
+    case usesFrontend:
+      return 'GOV.UK Frontend'
+    case usesLegacy:
+      return 'GOV.UK Template / Frontend Toolkit / Elements'
+    default:
+      return 'Unknown'
   }
-
-  return 'Unknown'
 }
